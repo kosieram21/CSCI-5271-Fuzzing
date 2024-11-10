d858f6f1386f12e1d1c94a17de914ad45173c715
kosieram21 <mitchkos21@gmail.com>
2024-11-10 16:37:35 -0600

    test driver

---
+++ driver.c
@@ -0,0 +1,24 @@
+#include "model_interface.h"
+
+int main() {
+    ModelInterface interface;
+    if (OpenInterface(&interface)) {
+        return -1;
+    }
+
+    char* output;
+    int outputSize;
+    if (GenerateMutation(&interface, &output, &outputSize)) {
+        return -1;
+    }
+
+    if (UpdateModel(&interface)) {
+        return -1;
+    }
+
+    if (CloseInterface(&interace)) {
+        return -1;
+    }
+    
+    return 0;
+}
