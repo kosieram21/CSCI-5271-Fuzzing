5d17b96e16e7f78547cb0c8b8cb5f7a8901093cc
kosieram21 <mitchkos21@gmail.com>
2024-11-14 16:35:01 -0600

    test open close

--- driver.c
+++ driver.c
@@ -6,12 +6,12 @@ int main() {
         return -1;
     }
 
-    unsigned char action;
-    if (GetAction(&interface, "test state", 10, &action)) {
-        return -1;
-    }
+    //unsigned char action;
+    //if (GetAction(&interface, "test state", 10, &action)) {
+    //    return -1;
+    //}
 
-    printf("action: %d", action);
+    //printf("action: %d", action);
 
     if (CloseInterface(&interface)) {
         return -1;
